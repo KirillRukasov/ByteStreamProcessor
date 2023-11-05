9f73311acabcb43cdfd29bf0e4967df1ba1bd4a2
kirillRukasov <shadowsun67@gmail.com>
2023-11-04 21:27:33 -0300

    Initial project structure setup

---
+++ ByteStreamProcessorLib/StreamProcessing/Interfaces/IStreamProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteStreamProcessorLib.StreamProcessing.Interfaces
+{
+    public interface IStreamProcessor
+    {
+        Task<IEnumerable<byte[]>> ProcessStreamAsync(Stream inputStream);
+    }
+}
